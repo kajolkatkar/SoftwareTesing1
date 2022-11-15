17a0751571a392c8f835945de120cc60b9cc5ca9
kajolkatkar <118249826+kajolkatkar@users.noreply.github.com>
2022-11-15 13:13:37 +0530

    Update class11.java

--- org.adharportal/src/test/java/GitHub/class11.java
+++ org.adharportal/src/test/java/GitHub/class11.java
@@ -7,8 +7,7 @@ public class class11 {
 		System.out.println("Universe");
 		
 		System.out.println("always");
-		System.out.println("with");
-		System.out.println("me");
+		
 		System.out.println("Thank you..");
 		
 		
